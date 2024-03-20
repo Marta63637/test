e466e2fedc0aa16ee1eaa3222ca2451082c2adbf
Marta <martagolotta@gmail.com>
2024-03-20 14:39:53 +0100

    Add operations

---
+++ operazioni.py
@@ -0,0 +1,15 @@
+def somma (a,b) --> int:
+    c = a + b
+    return c
+
+def divisione  (a,b) --> int:
+    c = a + b
+    return c
+
+def moltiplicazione (a,b) --> int:
+    c = a + b
+    return c
+    
+def sottrazione (a,b) --> int:
+    c = a + b
+    return c
